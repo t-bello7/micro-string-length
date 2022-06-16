7ef22091865518e3a9d973b847e41f06a4acf977
t-bello7 <bello4aus@gmail.com>
2022-06-16 08:23:57 +0100

    add task description to readme
    add a test to check if the string is at least 1 character long and not longer than 10 characters
    
    add character max and min check functionality

--- stringLength.js
+++ stringLength.js
@@ -1,3 +1,3 @@
-const stringLength = word =>  word.replace(/[\W_]/g, '').length
+const stringLength = word =>  (word.replace(/[\W_]/g, '').length > 0 && word.replace(/[\W_]/g, '').length < 10) ? word.replace(/[\W_]/g, '').length : 'string characters must be greater than 1 and less than 10';
 
 module.exports = stringLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,8 +1,9 @@
 const stringLength = require("./stringLength");
 
-test('StringLength --> count assertion', ()=> expect(stringLength('helli')).toBe(5))
+test('StringLength --> Count assertion', ()=> expect(stringLength('helli')).toBe(5))
 
 test('StringLength --> Count for strings including white space', () => expect(stringLength('hYt man')).toBe(6))
 
-test('empty string', ()=>expect(stringLength('')).toBe(0))
+test('StringLength --> String Character is greater than 1', ()=> expect(stringLength('')).toBe('string characters must be greater than 1 and less than 10'))
 
+test('StringLength --> String Character is less than 10', ()=> expect(stringLength('aabcdefghijklmn')).toBe('string characters must be greater than 1 and less than 10'))
